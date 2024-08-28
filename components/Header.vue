e2e6ad90399b2d7107266c782975944f7f7e396e
Dimasadhyaksa <143145756+Dimasadhyaksa@users.noreply.github.com>
2024-08-28 20:54:20 +0700

    Add files via upload

---
+++ components/Header.vue
@@ -0,0 +1,34 @@
+<template>
+    <nav class="navbar navbar-expand-lg bg-light shadow p-3 mb-5 bg-body rounded position-sticky">
+  <div class="container-fluid">
+    <img src="/assets/logo.png" alt="logo">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Features</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Pricing</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled">Disabled</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+</template>
+
+<style>
+#navbarNav{
+  padding-left: 30%;
+  font-size: 150%;
+
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,43 +1,78 @@
 <template>
-    <!-- navbar s -->
-        <nav class="navbar navbar-expand-lg bg-light position-sticky">
-    <div class="container-fluid">
-        <img src="/assets/logo.png" alt="logo">
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
+    <div id="carousel" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="/assets/gambar1.jpg" class="d-block w-100" alt="gambar1">
+            </div>
+            <div class="carousel-item">
+                <img src="/assets/gambar1.jpg" class="d-block w-100" alt="gambar2">
+            </div>
+            <div class="carousel-item">
+                <img src="/assets/gambar1.jpg" class="d-block w-100" alt="gambar3">
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls"
+            data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-        <div class="navbar-nav">
-            <a class="nav-link active" aria-current="page" href="#">Home</a>
-            <a class="nav-link" href="#">Features</a>
-            <a class="nav-link" href="#">Pricing</a>
-            <a class="nav-link disabled">Disabled</a>
-        </div>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls"
+            data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+    </div>
+
+    <!-- about -->
+    <div class="container">
+        <div class="row ">
+            <div class="col pt-5 d-flex">
+                <img src="/assets/gambar1.jpg" alt="" style="width: 40%; border-radius: 10%;">
+                <div class="col text-center">
+                    <h3>about</h3>
+                    <p>Rumah parfum merupan toko penyedia parfum local berkualitas premium</p>
+                </div>
+            </div>  
         </div>
     </div>
-    </nav>
-    <!-- navbar n -->
-    <!-- carousel s -->
-        <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
-    <div class="carousel-inner">
-        <div class="carousel-item active" data-bs-interval="10000">
-        <img src="/assets/gambar1.jpg" class="d-block w-100 h-50" alt="gambar1">
-        </div>
-        <div class="carousel-item" data-bs-interval="2000">
-        <img src="/assets/gambar1.jpg" class="d-block w-100" alt="...">
-        </div>
-        <div class="carousel-item">
-        <img src="/assets/gambar1.jpg" class="d-block w-100" alt="...">
+    <!-- about n  -->
+
+    <!-- product -->
+    <div class="container">
+        <div class="row">
+            <div class="col pt-5 text-center">
+                <h3>Product</h3>
+                <div class="col pt-4 d-flex d-flex justify-content-evenly">
+                <div class="card"  style="width: 18rem;">
+                    <img src="/assets/card.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of
+                            the card's content.</p>
+                    </div>
+                </div>
+                <div class="card"  style="width: 18rem;">
+                    <img src="/assets/card1.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of
+                            the card's content.</p>
+                    </div>
+                </div>
+                <div class="card"  style="width: 18rem;">
+                    <img src="/assets/card2.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of
+                            the card's content.</p>
+                    </div>
+                </div>
+                </div>
+            </div>
         </div>
     </div>
-    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Previous</span>
-    </button>
-    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Next</span>
-    </button>
-    </div>
-    <!-- carousel n -->
+    <!-- n -->
+    <!-- footer -->
+
+    <!-- footer -->
 </template>
+
+
+<style></style>
