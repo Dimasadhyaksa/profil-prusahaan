e47ae63a81773c705b6f4a7cb2cd848951873ef7
Dimasadhyaksa <143145756+Dimasadhyaksa@users.noreply.github.com>
2024-08-29 13:57:25 +0700

    Add files via upload

--- components/Header.vue
+++ components/Header.vue
@@ -1,34 +1,32 @@
 <template>
-    <nav class="navbar navbar-expand-lg bg-light shadow p-3 mb-5 bg-body rounded position-sticky">
-  <div class="container-fluid">
-    <img src="/assets/logo.png" alt="logo">
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Features</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Pricing</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled">Disabled</a>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
+     <nav class="navbar navbar-expand-lg navbar-light  shadow-sm fixed-top">
+      <div class="container">
+        <img src="~/assets/logo.png" alt="logo">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav ms-auto">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#about">about</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#product">Product</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">contact me</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
 </template>
 
 <style>
-#navbarNav{
-  padding-left: 30%;
-  font-size: 150%;
-
-}
+/* .navbar{
+  padding-right: 30%;
+} */
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -2,72 +2,69 @@
     <div id="carousel" class="carousel slide" data-bs-ride="carousel">
         <div class="carousel-inner">
             <div class="carousel-item active">
-                <img src="/assets/gambar1.jpg" class="d-block w-100" alt="gambar1">
-            </div>
-            <div class="carousel-item">
-                <img src="/assets/gambar1.jpg" class="d-block w-100" alt="gambar2">
-            </div>
-            <div class="carousel-item">
-                <img src="/assets/gambar1.jpg" class="d-block w-100" alt="gambar3">
+                <img src="~/assets/gambar1.jpg" class="d-block w-100" alt="gambar1">
             </div>
         </div>
-        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls"
-            data-bs-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Previous</span>
-        </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls"
-            data-bs-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Next</span>
-        </button>
     </div>
 
     <!-- about -->
-    <div class="container">
-        <div class="row ">
-            <div class="col pt-5 d-flex">
-                <img src="/assets/gambar1.jpg" alt="" style="width: 40%; border-radius: 10%;">
-                <div class="col text-center">
-                    <h3>about</h3>
-                    <p>Rumah parfum merupan toko penyedia parfum local berkualitas premium</p>
+    <section id="about" class="pt-5">
+        <div class="card mb-3" style="max-width: 80%;">
+            <div class="row g-0">
+                <div class="col-md-4">
+                    <img src="~/assets/about-image.jpeg" class="img-fluid rounded-start p-4" alt="...">
                 </div>
-            </div>  
-        </div>
-    </div>
-    <!-- about n  -->
-
-    <!-- product -->
-    <div class="container">
-        <div class="row">
-            <div class="col pt-5 text-center">
-                <h3>Product</h3>
-                <div class="col pt-4 d-flex d-flex justify-content-evenly">
-                <div class="card"  style="width: 18rem;">
-                    <img src="/assets/card.jpg" class="card-img-top" alt="...">
+                <div class="col-md-8">
                     <div class="card-body">
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of
-                            the card's content.</p>
+                        <h2 class="card-title text-center">about us</h2>
+                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to
+                            additional content. This content is a little bit longer.</p>
+                        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
                     </div>
-                </div>
-                <div class="card"  style="width: 18rem;">
-                    <img src="/assets/card1.jpg" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of
-                            the card's content.</p>
-                    </div>
-                </div>
-                <div class="card"  style="width: 18rem;">
-                    <img src="/assets/card2.jpg" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of
-                            the card's content.</p>
-                    </div>
-                </div>
                 </div>
             </div>
         </div>
-    </div>
+    </section>
+    <!-- about n  -->
+
+    <!-- product -->
+    <section id="product">
+        <div class="container">
+            <div class="row text-center">
+                <div class="col pt-5">
+                    <h2>Product</h2>
+                </div>
+            </div>
+            <div class="row d-flex justify-content-around pt-5">
+                <div class="card" style="width: 18rem;">
+                    <img src="~/assets/jaguar.jpeg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title text-center">Jaguar</h5>
+                        <p class="card-text">IDR 285.000</p>
+                        <a href="#" class="btn btn-primary">Open</a>
+                    </div>
+                </div>
+                <div class="card" style="width: 18rem;">
+                    <img src="~/assets/avrill.jpeg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title text-center">avrill</h5>
+                        <p class="card-text">IDR 285.000</p>
+                        <a href="#" class="btn btn-primary">Open</a>
+                    </div>
+                </div>
+                <div class="card" style="width: 18rem;">
+                    <img src="~/assets/bulgary.jpeg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title text-center">Jaguar</h5>
+                        <p class="card-text">IDR 285.000</p>
+                        <a href="#" class="btn btn-primary">Open</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
     <!-- n -->
     <!-- footer -->
 
@@ -75,4 +72,6 @@
 </template>
 
 
-<style></style>
+<style>
+
+</style>
