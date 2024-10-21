5dc8056de7410380fe44ece76196153a31350229
Dimasadhyaksa <143145756+Dimasadhyaksa@users.noreply.github.com>
2024-10-21 08:33:38 +0700

    Add files via upload

--- components/Header.vue
+++ components/Header.vue
@@ -1,29 +1,33 @@
 <template>
-     <nav class="navbar navbar-expand-lg navbar-light  shadow-sm fixed-top">
-      <div class="container">
-        <img src="~/assets/logo.png" alt="logo">
-        <div class="collapse navbar-collapse" id="navbarNav">
-          <ul class="navbar-nav ms-auto">
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#">Home</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#about">about</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#product">Product</a>
-            </li>
-            <!-- <li class="nav-item">
+  <nav class="navbar navbar-expand-lg  shadow-sm fixed-top" >
+    <div class="image">
+
+      <img src="~/assets/logo.png" alt="logo">
+    </div>
+    <div class="container">
+
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ms-auto">
+          <li class="nav-item">
+            <a class="nav-link" aria-current="page" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#about">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#product">Product</a>
+          </li>
+          <!-- <li class="nav-item">
               <a class="nav-link" href="#">contact me</a>
             </li> -->
-          </ul>
-        </div>
+        </ul>
       </div>
-    </nav>
+    </div>
+  </nav>
 </template>
 
 <style>
-/* .navbar{
-  padding-right: 30%;
-} */
+.nav-link:active::content {
+  text-decoration: underline;
+}
 </style>
